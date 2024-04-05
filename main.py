d9c0c27b6d4a9ca739be3c7bf9a9f1c9c714795f
Rooshi <139238955+ItsRooshi@users.noreply.github.com>
2024-04-05 11:46:13 +0200

    modification fonctionelle

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""
 import arrr
 from pyscript import document
 
@@ -7,3 +8,42 @@ def translate_english(event):
     english = input_text.value
     output_div = document.querySelector("#output")
     output_div.innerText = arrr.translate(english)
+"""
+
+
+# imports
+import time  # for measuring time duration of API calls
+from openai import OpenAI
+import os
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY","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"))
+
+
+input_text = document.querySelector("#english")
+english = input_text.value
+
+# record the time before the request is sent
+start_time = time.time()
+
+# send a ChatCompletion request to count to 100
+response = client.chat.completions.create(
+    model='gpt-3.5-turbo',
+    messages=[
+        {'role': 'user', 'content': english}
+    ],
+    temperature=0,
+    stream=True  # again, we set stream=True
+)
+output_div = document.querySelector("#output")
+output_div.innerText = arrr.translate(response)
+# create variables to collect the stream of chunks
+collected_chunks = []
+collected_messages = []
+
+# iterate through the stream of events
+for chunk in response:
+    chunk_time = time.time() - start_time  # calculate the time delay of the chunk
+    collected_chunks.append(chunk)  # save the event response
+    chunk_message = chunk.choices[0].delta.content  # extract the message
+    if(chunk_message != None):
+        collected_messages.append(chunk_message)  # save the message
+        print(chunk_message, end="")
