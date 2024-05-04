ed9283d066b4bbf262609a8ea8172bd6e690138e
BuildTools <unconfigured@null.spigotmc.org>
2024-05-04 15:20:06 +0200

    reset

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-
 import arrr
 from pyscript import document
 
-def test(event):
+
+def translate_english(event):
     input_text = document.querySelector("#english")
     english = input_text.value
     output_div = document.querySelector("#output")
